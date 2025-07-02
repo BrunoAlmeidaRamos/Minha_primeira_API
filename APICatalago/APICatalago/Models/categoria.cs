9414d5e0f8d150fb98707d142c4b981ef7796771
Bruno Ramos <robrumi01@gmail.com>
2025-07-02 11:30:15 -0300

    Data Annotations

--- APICatalago/APICatalago/Models/categoria.cs
+++ APICatalago/APICatalago/Models/categoria.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections.ObjectModel;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace APICatalago.Models;
 
+    [Table("Categoria")]
 public class categoria
 {
-
+    
     public categoria()
     {
         Produtos = new Collection<produto>();
     }
+
+    [Key]
     public int CategoriaId { get; set; }
+
+    [StringLength(80)]
+    [Required]
     public string? Nome { get; set; }
+
+    [StringLength(300)]
+    [Required]
     public string? ImagemUrl { get; set; }
 
     public ICollection<produto>? Produtos { get; set; }
--- APICatalago/APICatalago/Models/produto.cs
+++ APICatalago/APICatalago/Models/produto.cs
@@ -1,11 +1,28 @@
-﻿namespace APICatalago.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+namespace APICatalago.Models;
 
+    [Table("Produtos")]
 public class produto
 {
+    [Key]
     public int ProdutoId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? descricao { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal (10,2)")]
     public decimal preco { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? imagemUrl { get; set; }
     public bool Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
