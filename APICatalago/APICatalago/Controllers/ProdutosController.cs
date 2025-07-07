fafaa8de0568f99fc8c6889a55b2b37fa3ea423d
Bruno Ramos <robrumi01@gmail.com>
2025-07-07 11:25:15 -0300

    Iniciando os Controllers

---
+++ APICatalago/APICatalago/Controllers/ProdutosController.cs
@@ -0,0 +1,19 @@
+﻿using APICatalago.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalago.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
