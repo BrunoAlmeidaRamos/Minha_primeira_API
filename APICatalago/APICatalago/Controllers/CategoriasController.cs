9c8153daa18924b93b3e269a07a7d5e6f3481fc4
Bruno Ramos <robrumi01@gmail.com>
2025-07-09 16:32:18 -0300

    CategoriasController

---
+++ APICatalago/APICatalago/Controllers/CategoriasController.cs
@@ -0,0 +1,91 @@
+﻿using APICatalago.Context;
+using APICatalago.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalago.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("produtos")]
+
+        public ActionResult<IEnumerable<categoria>> GetCategoriasProdutos()
+        {
+            return _context.Categorias.Include(p => p.Produtos).ToList();
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<categoria>> Get()
+        {
+            var categoria = _context.Produtos.ToList();
+            if (categoria is null)
+            {
+                return NotFound("Produtos não econtrados");
+            }
+            return Ok(categoria);
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Post(categoria categoria)
+        {
+            if (categoria is null)
+                return BadRequest("Produto inválido");
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult put(int id, categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+
+            return Ok("Produto removido com sucesso");
+
+        }
+    }
+}
